de2d6df285018f1b9b8d2f2a9b52bcf8ded1dcbb
Nikita60012 <ivanov.nikita600@yandex.ru>
2023-11-03 17:51:26 +0300

    Add logs, argparse

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import os
 import cv2
 import dlib
+import argparse
 from skimage import io
 import matplotlib.pyplot as plt
 from scipy.spatial import distance
+import logging
 
-CONST_PATH_TO_LEARNING_PHOTOS = "C://faces"  # Путь к фотографиям, с которыми будет проводится сравнение
-CONST_PATH_TO_SEARCHING_PHOTO = "C://search"  # Путь к фото, соответствие которого нужно найти
-CONST_PATH_TO_DATA = os.path.dirname(os.path.realpath(__file__)) + '\\data'  # Путь к весам
+logging.basicConfig(level=logging.INFO, filename='face_search_log.log', filemode='a',
+                    format="%(asctime)s %(levelname)s %(message)s")
+
+parser = argparse.ArgumentParser(description='pathes to data and faces')
+parser.add_argument('data_dir', type=str, help='Path to data')
+parser.add_argument('learn_dir', type=str, help='Path to faces')
+parser.add_argument('search_dir', type=str, help='Path to recognizing face')
+args = parser.parse_args()
 
 # Подгрузка весов и инициализация детектора лица
-sp = dlib.shape_predictor(f'{CONST_PATH_TO_DATA}\\shape_predictor_68_face_landmarks.dat')
-facerec = dlib.face_recognition_model_v1(f'{CONST_PATH_TO_DATA}\\dlib_face_recognition_resnet_model_v1.dat')
+sp = dlib.shape_predictor(f'{args.data_dir}\\shape_predictor_68_face_landmarks.dat')
+facerec = dlib.face_recognition_model_v1(f'{args.data_dir}\\dlib_face_recognition_resnet_model_v1.dat')
 detector = dlib.get_frontal_face_detector()
 
 
 class FaceDetector:
-    def __init__(self, pathToFaces, pathToFace, pathToData):
-        self.path_to_faces = pathToFaces
-        self.path_to_face = pathToFace
-        self.path_to_data = pathToData
 
     # Нахождение дескриптора каждой из фотографии, представленной в базе данных
     def find_face_descriptors(self, path: str):
@@ -34,12 +37,12 @@ class FaceDetector:
                 for k, d in enumerate(dets): shape = sp(images, d)
                 descriptors.append(facerec.compute_face_descriptor(images, shape))
                 progress = str(round((100 / len(faces)) * count))
-                print("Загружено фото в базу: {}% из 100%".format(progress))
-            print("Все фото успешно загружены!")
+                logging.info("Загружено фото в базу: {}% из 100%".format(progress))
+            logging.info("Все фото успешно загружены!")
             return descriptors, faces
 
         except:
-            print("Что-то пошло не так. Возможно вы не правильно указали путь к папке с фотографиями.")
+            logging.exception("Что-то пошло не так. Возможно вы не правильно указали путь к папке с фотографиями.")
 
     # Нахождение дескриптора фотографии для идентификации
     def find_main_descriptor(self, path: str):
@@ -53,34 +56,40 @@ class FaceDetector:
     # Поиск минимальнейшего расстояния между дескрипторами
     def comparison(self, path: str, descriptors, descriptor):
         distances = []
+        result: str
         for i in descriptors: distances.append(distance.euclidean(descriptor, i))
         min_dist = min(distances)
         image2 = io.imread(f'{path}' + '/' + faces[distances.index(min_dist)])
         if min_dist > 0.6 and len(face_descriptors) == 1:
-            print('На фотографиях разные люди!')
+            logging.info('На фотографиях разные люди!')
+            result = 'На фотографиях разные люди!'
         elif min_dist <= 0.6 and len(face_descriptors) > 1:
-            print('Полученное лицо совпадает с лицом на фото ' + faces[distances.index(min_dist)])
+            logging.info('Полученное лицо совпадает с лицом на фото ' + faces[distances.index(min_dist)])
+            result = 'Полученное лицо совпадает с лицом на фото'
         elif min_dist > 0.6 and len(face_descriptors) > 1:
-            print('Точно сказать не могу, но наверное это человек с фотографии ' + faces[distances.index(min_dist)])
-        print('Эвклидово расстояние между дескрипторами: ' + str(min_dist) + '\n')
-        return image2
+            logging.info('Точно сказать не могу, но наверное это человек с фотографии '
+                         + faces[distances.index(min_dist)])
+            result = 'Точно сказать не могу, но наверное это человек с фотографии'
+        logging.info('Эвклидово расстояние между дескрипторами: ' + str(min_dist) + '\n')
+        return image2, result
 
     # Отрисовка результата
-    def draw_result(self, detect, image, image2):
+    def draw_result(self, detect, image, image2 , result):
         ltrb = [detect[0].tl_corner(), detect[0].br_corner()]
         plt.figure(figsize=(10, 6))
         cv2.rectangle(image, (ltrb[0].x, ltrb[0].y, ltrb[1].x, ltrb[1].y), (255, 0, 0), 5)
         plt.subplot(1, 2, 1)
-        plt.title(f"Полученное лицо:")
+        plt.title('Полученное лицо:')
+        plt.xlabel(result)
         plt.imshow(image)
         plt.subplot(1, 2, 2)
-        plt.title("Предполагаемое лицо:")
+        plt.title('Предполагаемое лицо:')
         plt.imshow(image2)
         plt.show()
 
 
-face_detect = FaceDetector(CONST_PATH_TO_LEARNING_PHOTOS, CONST_PATH_TO_SEARCHING_PHOTO, CONST_PATH_TO_DATA)
-face_descriptors, faces = face_detect.find_face_descriptors(CONST_PATH_TO_LEARNING_PHOTOS)
-main_descriptor, face, img, detection = face_detect.find_main_descriptor(CONST_PATH_TO_SEARCHING_PHOTO)
-img2 = face_detect.comparison(CONST_PATH_TO_LEARNING_PHOTOS, face_descriptors, main_descriptor)
-face_detect.draw_result(detection, img, img2)
+face_detect = FaceDetector()
+face_descriptors, faces = face_detect.find_face_descriptors(args.learn_dir)
+main_descriptor, face, img, detection = face_detect.find_main_descriptor(args.search_dir)
+img2, result = face_detect.comparison(args.learn_dir, face_descriptors, main_descriptor)
+face_detect.draw_result(detection, img, img2, result)

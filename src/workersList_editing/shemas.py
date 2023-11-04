51f45fcfe012f7746d13de27e0ab0be1fe8cdcc6
Nikita60012 <ivanov.nikita600@yandex.ru>
2023-11-04 21:17:50 +0300

    Edit workers in database

---
+++ src/workersList_editing/shemas.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class AddWorker(BaseModel):
+    id: int
+    fullname: str
+    birthdate: datetime
+    phone: str
+
+
+class UpdateWorker(BaseModel):
+    id: int
+    fullname: str
+    birthdate: datetime
+    phone: str

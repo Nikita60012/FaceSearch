c1045aadcfda5ff23f278b31538a5462e76b248f
Администратор <ivanov.nikita600@yandex.ru>
2023-11-08 12:42:39 +0300

    begin make identificate

---
+++ src/identificate/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import MetaData, Integer, String, TIMESTAMP, Table, Column, LargeBinary
+
+metadata = MetaData()
+
+worker_identifications = Table(
+    'worker_identifications',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('fullname', String, nullable=True),
+    Column('date', TIMESTAMP, nullable=True),
+    Column('worker_photo', LargeBinary, nullable=True),
+    Column('unknown', LargeBinary, nullable=True)
+)

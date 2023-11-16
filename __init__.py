c3724c6c373616ff8af803c907506816c817a802
Nikita60012 <ivanov.nikita600@yandex.ru>
2023-11-16 15:21:11 +0300

    Launch from cmd

--- __init__.py
+++
@@ -1,7 +0,0 @@
-import src
-from src.dao import IdentificateDAO, WorkersListModelsDAO
-from src.database.connection import database, db_config
-from src.dto.request import AddWorkerDTO, UpdateWorkerDTO
-from src.rest.router import IdentificateRouter, WorkersListRouter
-from src.service.detector import DetectorService
-from src.service.Image import ImageService

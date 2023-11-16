3c546bd4c37574a9c63f0d52a4470e5da78f01e9
Nikita60012 <ivanov.nikita600@yandex.ru>
2023-11-16 15:30:04 +0300

    Launch from cmd

--- src/service/detector/DetectorService.py
+++ src/service/detector/DetectorService.py
@@ -7,8 +7,9 @@ from PIL import Image
 from scipy.spatial import distance
 import logging
 
-dlib_download_model = 'src\\resource\\dlib_model.dat'
-dlib_download_landmark = 'src\\resource\\dlib_landmark.dat'
+dlib_data_path = 'src\\resource\\'
+dlib_download_model = f'{dlib_data_path}dlib_model.dat'
+dlib_download_landmark = f'{dlib_data_path}dlib_landmark.dat'
 
 
 class FaceDetector:
@@ -90,5 +91,6 @@ class FaceDetector:
         if not (os.path.isfile(dlib_download_model) and os.path.isfile(dlib_download_landmark)):
             model_url = 'https://drive.google.com/u/0/uc?id=1IgUL8X7jb0bDXow0JZZJwHNB-f-Jo00x&export=download'
             landmark_url = 'https://drive.google.com/u/0/uc?id=1fCHIUgpwmcK5iHMtD6r6a6D0NUSKNcvf&export=download'
-            wget.download(model_url, 'src\\resource\\')
-            wget.download(landmark_url, 'src\\resource\\')
+            os.mkdir(dlib_data_path)
+            wget.download(model_url, dlib_data_path)
+            wget.download(landmark_url, dlib_data_path)
